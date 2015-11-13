f1c4eacedc4223493de9564b4929a16846f70473
Jeffrey Jen <yihungjen@gmail.com>
2015-11-13 09:31:10 +0000

    NEW: subcommand client

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
+set -e
 
-if [ -f /etc/environ ]; then
-    # setup necessary host environments
-    . /etc/environ
-fi
+COMMAND="${1}"; shift 1
 
-# pull down init script from launch-config/tlsproxy
-URL="https://raw.githubusercontent.com/AndrewMagv/aws-devops/${REF}"
-curl -sSL -O ${URL}/proxy-init.sh
-chmod +x proxy-init.sh
+case "${COMMAND}" in
+    server)
+        if [ -f /etc/environ ]; then
+            # setup necessary host environments
+            . /etc/environ
+        fi
 
-# Keep our baseline configuration
-cat /proxy.conf >/proxy.init.conf
+        # pull down init script from launch-config/tlsproxy
+        URL="https://raw.githubusercontent.com/AndrewMagv/aws-devops/${REF}"
+        curl -sSL -O ${URL}/proxy-init.sh
+        chmod +x proxy-init.sh
 
-# launch proxy init
-NODE_REGION=${NODE_REGION} ./proxy-init.sh $@
+        # Keep our baseline configuration
+        cat /proxy.conf >/proxy.init.conf
 
-# invoke stunnel with TLS proxy config
-# NOTE: proxy.conf is our baseline proxy config, proxy-init appends per service
-# configuration at the end to make proxy.init.conf
-exec /usr/bin/stunnel4 /proxy.init.conf
+        # launch proxy init
+        NODE_REGION=${NODE_REGION} ./proxy-init.sh $@
+
+        # invoke stunnel with TLS proxy config
+        # NOTE: proxy.conf is our baseline proxy config, proxy-init appends per service
+        # configuration at the end to make proxy.init.conf
+        exec /usr/bin/stunnel4 /proxy.init.conf
+        ;;
+
+    client)
+        if [ -e /client.conf ]; then
+            exec /usr/bin/stunnel4 /client.conf
+        else
+            echo "No client config; abort"; exit 1
+        fi
+        ;;
+
+    *)
+        exec "$@"
+        ;;
+esac
